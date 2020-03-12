dfa68cec48dab01a8856d17d84324af7e9b8b9d7
Jules Youngberg <julesdayoungberg@gmail.com>
2020-03-11 21:05:21 -0700

    add deploy github action

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     extensions: ['*', '.js']
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
   devtool: 'inline-source-map',
