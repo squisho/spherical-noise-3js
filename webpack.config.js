da354482478349503187e2e6a9eb1bcaa95081d1
Jules Youngberg <julesdayoungberg@gmail.com>
2020-05-03 21:07:23 -0700

    prettier webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require('path');
+const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js']
-  },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-  },
-  devtool: 'inline-source-map',
-  mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: false // was running 2 apps simultaneously, this prevents that.
-    })
-  ]
-};
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    devtool: 'inline-source-map',
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: false, // was running 2 apps simultaneously, this prevents that.
+        }),
+    ],
+}
